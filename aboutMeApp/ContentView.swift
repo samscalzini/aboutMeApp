7cce40d79e055998b7197d6dd9f58d5505250c8d
Samantha Scalzini <sgscalzini@gmail.com>
2023-06-30 10:00:46 -0400

    About Me app

--- aboutMeApp/ContentView.swift
+++ aboutMeApp/ContentView.swift
@@ -6,43 +6,63 @@
 //
 
 import SwiftUI
-
+import UserNotifications
 struct ContentView: View {
     @State private var facts = ("I am a rising senior from Atlanta, Georgia at Lakeside High School. ")
-    
+ //   @State private var dates = Set<DateComponents>()
+  //  @State private var dates = Set<DateComponents>()
     var body: some View {
-        
-        VStack {
-            Text("All About Me!")
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundColor(Color(hue: 0.729, saturation: 0.786, brightness: 0.804))
+        ScrollView {
+            
+            
+            VStack {
+                Text("All About Me")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.729, saturation: 0.786, brightness: 0.804))
                 
-            Image("meandtaylor")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Image("meclimbing")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+             //   MultiDatePicker(selection: $dates, label: {Text("n")})
             
-            
-            //     Spacer()
-            Text("Samantha Scalzini")
-                .font(.title)
-                .foregroundColor(Color(hue: 0.723, saturation: 0.609, brightness: 0.914))
-            Text("\(facts)")
-                .multilineTextAlignment(.center)
-            Button("Learn More About Me") {
-               facts = ("I love listening to music, rock climbing and spending time with my family")
+                
+                
+                //        UIDatePickerModeDate}
+                //    startDate
+                //    datePickerMode
+                // Text("\(dates)")
+                //   startDate = dates(format: date)
+                Image("meandtaylor")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Image("meclimbing")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                
+                //     Spacer()
+                Text("Samantha Scalzini")
+                    .font(.title)
+                    .foregroundColor(Color(hue: 0.723, saturation: 0.609, brightness: 0.914))
+                Text("\(facts)")
+                    .multilineTextAlignment(.center)
+                Button("Learn More About Me") {
+                    facts = ("I love listening to music, rock climbing and spending time with my family")
                 }
-           
             } //end of button
             .buttonStyle(.borderedProminent)
             .tint(.purple)
             //end of view stack
             .padding()
-        } // end of v stack
-    } //end of body
+            
+            
+        }
+        }
+    } // end of v stack]
+   // var format = dates().formatted(date : .abbreviated, time : .omitted)
+   
+    
+    
+//end of body
  //end of content view ?
         struct ContentView_Previews: PreviewProvider {
             static var previews: some View {
